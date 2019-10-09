d1fdfa6227113f63b50fc3fa9e1620113ac8d108
Erik Gúth <guthember@gmail.com>
2019-10-09 12:23:32 +0200

    #1, #2: Egy meccset tud már játszani

--- Program.cs
+++ Program.cs
@@ -14,8 +14,86 @@ namespace KoPapirOllo
             // Papír legyőzi a  követ
             // Olló  legyőzi a  papírt
             Console.WriteLine("Kő - Papír - Olló játék");
+            
+            // számítógép lehetséges választásai
+            string[] valasztas = new string[] { 
+                "Kő",
+                "Papír",
+                "Olló"
+            };
 
+            // véletlen szám 0,1,2
+            Random vel = new Random();
+            int mit = vel.Next(0, 3);
 
+            // Ellenőrzés! a végleges kódba nem kell
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    mit = vel.Next(0, 3);
+            //    Console.WriteLine(valasztas[mit]);
+            //}
+            Console.WriteLine("Kérek egy lehetőséget: ");
+            Console.WriteLine("Kő -> 1, Papír -> 2, Olló -> 3");
+            int felhMit = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            // Ellenőrzés! a végleges kódba nem kell
+            //Console.WriteLine(valasztas[felhMit]);
+
+            // Eredmény kiértékelése
+            string nyert = "";
+            
+            // számítógép Kő
+            if (mit == 0)
+            {
+                if (felhMit == 0)
+                {
+                    nyert = "Döntetlen";
+                }
+                else if (felhMit == 1)
+	            {
+                    nyert = "Ember";
+	            }
+                else
+                {
+                    nyert = "Gép";
+                }
+            }
+            // számítógép Papír
+            else if (mit == 1)
+            {
+                if (felhMit == 0)
+                {
+                    nyert = "Gép";
+                }
+                else if (felhMit == 1)
+                {
+                    nyert = "Döntetlen";
+                }
+                else
+                {
+                    nyert = "Ember";
+                }  
+            }
+            // számítógé Olló
+            else
+            {
+                if (felhMit == 0)
+                {
+                    nyert = "Ember";
+                }
+                else if (felhMit == 1)
+                {
+                    nyert = "Gép";
+                }
+                else
+                {
+                    nyert = "Döntelen";
+                }  
+            }
+
+            Console.WriteLine("Gép: {0}", valasztas[mit]);
+            Console.WriteLine("Ember: {0}", valasztas[felhMit]);
+            Console.WriteLine(nyert);
 
             Console.ReadKey();
         }
--- README.md
+++ README.md
@@ -4,11 +4,24 @@
 - Kõ    legyõzi az ollót
 - Papír legyõzi a  követ
 - Olló  legyõzi a  papírt
+- ugyanaz döntetlen
 
 ## Játékmenet
-- számítógép gondol az egyikre (véletlenszám)
-- a felhasználó megadja a saját lehetõségét
-  - (K)õ, (P)apír, (O)lló
+- [x] számítógép gondol az egyikre (véletlenszám)
+- [x]a felhasználó megadja a saját lehetõségét
+  - (K)õ -> 1, (P)apír -> 2, (O)lló -> 3
+  - feltételezzük, hogy jó értékeket ad meg!!!
 - eredmény kiértékelése
 - eredmény kiírása
 - 3 nyert játsza -> játék megnyerése
+
+## Megoldás
+### Számítógép gondol valamire
+- [x] Tömb -> tartalmazza = {"Kõ","Papír", "Olló"}
+- [x] Véletlen szám elõállítása (0,1,2)
+- [x] Kiíratjuk a számítógép "választását"
+
+### Felhasználó választása
+- [x] Kérjünk be egy számot (1,3) 
+- [x] Írassuk ki a választást
+
