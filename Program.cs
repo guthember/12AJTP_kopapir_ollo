4987a804ca2db369e545075c77be624f30f4d6ec
Erik Gúth <guthember@gmail.com>
2019-10-09 11:57:00 +0200

    #1, #2: Kezdés

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoPapirOllo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kő    legyőzi az ollót
+            // Papír legyőzi a  követ
+            // Olló  legyőzi a  papírt
+            Console.WriteLine("Kő - Papír - Olló játék");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Kõ - papír - olló játék
+
+## Játékszabály
+- Kõ    legyõzi az ollót
+- Papír legyõzi a  követ
+- Olló  legyõzi a  papírt
+
+## Játékmenet
+- számítógép gondol az egyikre (véletlenszám)
+- a felhasználó megadja a saját lehetõségét
+  - (K)õ, (P)apír, (O)lló
+- eredmény kiértékelése
+- eredmény kiírása
+- 3 nyert játsza -> játék megnyerése
